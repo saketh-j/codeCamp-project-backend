3f9b5d5f2056eaa6ee952fbb8913c92f7d91515a
saketh <sakethsid@gmail.com>
2023-07-28 13:17:48 +0530

    initial commit

---
+++ db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+mongoose.connection.on('error', (error) => {
+  console.error('Error connecting to MongoDB:', error);
+});
---
+++ index.js
@@ -0,0 +1,60 @@
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('./db.js')
+const User = require('./models/user.model')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const PORT = process.env.PORT || 8000
+
+app.use(cors())
+app.use(express.json())
+
+
+app.post('/api/signup', async (req, res) => {
+	console.log(req.body)
+	try {
+		const newPassword = await bcrypt.hash(req.body.password, 10)
+		await User.create({
+			name: req.body.name,
+			email: req.body.email,
+			password: newPassword,
+		})
+		res.json({ status: 'ok' })
+	} catch (err) {
+		res.json({ status: 'error', error: 'Duplicate email' })
+	}
+})
+
+app.post('/api/signin', async (req, res) => {
+	const user = await User.findOne({
+		email: req.body.email,
+	})
+	if (!user) {
+		return { status: 'error', error: 'Invalid Signin' }
+	}
+	const isPasswordValid = await bcrypt.compare(
+		req.body.password,
+		user.password
+	)
+	if (isPasswordValid) {
+		const token = jwt.sign(
+			{
+				name: user.name,
+				email: user.email,
+			},
+			'secret123'
+		)
+		return res.json({ status: 'ok', user: token })
+	} else {
+		return res.json({ status: 'error', user: false })
+	}
+});
+
+
+
+app.listen(PORT, () => {
+	console.log(`Server started on port ${PORT}`)
+})
